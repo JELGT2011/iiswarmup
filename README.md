f47b8843e8966b947e347b6d044c5771173714ea
Chris Adams <chris@cadams.com.au>
2012-08-30 10:24:38 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 iiswarmup
 =========
 
-A windows service which keeps IIS app pools warm.
+A windows service which keeps IIS app pools warm. Currently very rudimentary; can only warm up one app pool by a configured URL, and then the service will stop.
+
+This tool is not actively maintained, but if you'd like some features I'm happy to add them, just ask.
+
+**NOTE:** This is only useful with IIS 7 and earlier; IIS 7.5 and onwards provide auto-start. For more information, please see [http://msdn.microsoft.com/en-us/library/ee677285(v=azure.10).aspx](http://msdn.microsoft.com/en-us/library/ee677285(v=azure.10).aspx)
+## Installation ##
+
+1. Download somewhere
+2. Edit the config and add your URL (one URL supported for now; for multiple URLs you currently have to follow these steps multiple times)
+3. Open an administrative command prompt (click Start, type cmd then press Ctrl-Shift-Enter and accept the UAC prompt) and install the service:
+
+    sc create "iiswarmup" binPath= "D:\Projects\iiswarmup\bin\De
+bug\iiswarmup.exe" start= auto DisplayName= "IIS Warmup - My Site"
+
+4. Start the service manually if needed; it will start the next time the server is rebooted.
+
+## TODO ##
+
+- Support multiple sites/AppPools
+- Find a way to monitor AppPool(s) and warm up immediately after recycle
+- Option to read IIS bindings rather than requiring the user to specify a URL
+- Run warmup in a worker thread to avoid service startup timeouts
---
+++ Service1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.ServiceProcess;
+
+namespace iiswarmup
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            var url = ConfigurationManager.AppSettings["url"];
+            using (var lw = new StreamWriter(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "iiswarmup.log"), true))
+            {
+                try
+                {
+                    var start = DateTime.Now;
+                    using (var rsp = WebRequest.Create(url).GetResponse() as HttpWebResponse)
+                    {
+                        var tm = DateTime.Now - start;
+                        if (rsp == null)
+                        {
+                            lw.WriteLine("{0} [pid:{1}] Hit {2}; took {3}ms; response is null", DateTime.Now,
+                                         Process.GetCurrentProcess().Id, url, tm.TotalMilliseconds);
+                        }
+                        else
+                        {
+                            lw.WriteLine("{0} [pid:{1}] Hit {2}; took {3}ms; response code {4}; response length {5}",
+                                         DateTime.Now,
+                                         Process.GetCurrentProcess().Id, url, tm.TotalMilliseconds, rsp.StatusCode, rsp.ContentLength);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    lw.WriteLine("{0} [pid:{1}] Hit {2}; got exception {3}: {4}\nStack: {5}{6}", DateTime.Now,
+                        Process.GetCurrentProcess().Id, url, ex.GetType(), ex.Message, ex.StackTrace,
+                                 ex.InnerException == null
+                                     ? ""
+                                     : string.Format("\nInner: {0}: {1}\nInner stack: {2}", ex.InnerException.GetType(),
+                                                     ex.InnerException.Message, ex.InnerException.StackTrace));
+                }
+            }
+            Stop();
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
